0238562c85e334ae4211f15e98386cc965fe58c6
zutshi <aaditya.zutshi@gmail.com>
2016-03-21 11:10:41 -0600

    updated

--- err.py
+++ err.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
+import inspect
 
 from blessed import Terminal
 
@@ -12,21 +15,33 @@ class Fatal(Exception):
     pass
 
 
-def error(msg):
-    print term.red('ERROR: ' + msg)
-    exit(-1)
+# def error(msg):
+#     print term.red('ERROR: ' + msg)
+#     exit(-1)
+
+WARN_STR = '{} :{}::WARNING: {}'
 
 
 def warn(msg):
-    print term.red_on_white('WARNING: ' + msg)
+    callers_frame_idx = 1
+    (frame, filename, lineno,
+     function_name, lines, index) = inspect.getouterframes(
+                                         inspect.currentframe())[callers_frame_idx]
+    msg_ = WARN_STR.format(filename, lineno, msg)
+    print(term.red_on_white(msg_))
 
 
 def warn_severe(msg):
-    print term.red_on_white('WARNING: ' + msg)
+    callers_frame_idx = 1
+    (frame, filename, lineno,
+     function_name, lines, index) = inspect.getouterframes(
+                                         inspect.currentframe())[callers_frame_idx]
+    msg_ = WARN_STR.format(filename, lineno, msg)
+    print(term.red_on_white(msg_))
     # forces the user to take heed!
     raw_input('please acknowledge by pressing enter')
 
 
-def int_error(msg):
-    print 'INTERNAL ERROR: ' + msg
-    exit(-1)
+# def int_error(msg):
+#     print 'INTERNAL ERROR: ' + msg
+#     exit(-1)
